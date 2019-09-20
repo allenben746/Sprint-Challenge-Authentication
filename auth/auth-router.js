f9eecefae97e32133b5c970a84a787c40cdecf8f
Ben <allenben@protonmail.com>
2019-09-20 11:09:50 -0500

    Added router, middleware, model, and edited knexfile

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,53 @@
+const Users = require('./authModel');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const secret = require('../config/secret');
+
 const router = require('express').Router();
 
+
 router.post('/register', (req, res) => {
-  // implement registration
+    let user = req.body;
+    const hash =  bcrypt.hashSync(user.password, 12);
+    user.password = hash;
+
+    Users.addUsers(user)
+        .then(addedUser => {
+            res.status(201).json(addedUser);
+        })
+        .catch(err => {
+            res.status(500).json({message: "Error registering. Try again."});
+        })
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+    const { username, password } = req.body;
+  
+    Users.findByUser({ username })
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                const token = getToken(user);
+                res.status(200).json({message: `Logged in.`, token});
+            } else {
+                res.status(401).json({ message: 'Invalid Credentials' });
+            };
+        }).catch(err => {
+            res.status(500).json({ message: 'Error logging in. Try again.'});
+        })
 });
+
+function getToken(user){
+    const payload = {
+        subject: user.id,
+        username: user.username,
+        jwtid: user.id
+    };
+    const options = {
+        expiresIn: '3h',
+    };
+    return jwt.sign(payload, secret.secret, options);
+}
 
 module.exports = router;
